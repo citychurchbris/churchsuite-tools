2e00f977476157906d027779ad0eb4574bc7a9d9
Matthew Sital-Singh <mattss@netsight.co.uk>
2017-01-27 11:24:44 +0000

    Add church name to config

--- masterrota.py
+++ masterrota.py
@@ -4,24 +4,25 @@ from lxml import html
 from datetime import datetime
 
 
-def fetch_overview(username, password, year=None):
+def fetch_overview(churchname, username, password, year=None):
     if year is None:
         year = datetime.today().year
     fromdate = '01-01-{}'.format(year)
     todate = '01-01-{}'.format(year+1)
 
-    report_url = "https://citychurchbris.churchapp.co.uk/modules/rotas/reports/rotas_overview.php?date_start={fromdate}&date_end={todate}&order_by=default&submit_btn=Generate"  # noqa
+    report_url = "https://{churchapp}.churchapp.co.uk/modules/rotas/reports/rotas_overview.php?date_start={fromdate}&date_end={todate}&order_by=default&submit_btn=Generate"  # noqa
     login_url = "https://login.churchapp.co.uk/"
     s = requests.Session()
     s.post(
         login_url,
         data={
+            'churchname': churchname,
             'username': username,
             'password': password,
             'system': 'churchapp',
             },
         cookies={
-            'churchapp_login_account': 'citychurchbris'
+            'churchapp_login_account': churchname
         }
     )
     response = s.get(report_url.format(
@@ -35,7 +36,10 @@ def fetch_overview(username, password, year=None):
 if __name__ == "__main__":
     with open('config.json', 'r') as f:
         config = json.load(f)
-    overview = fetch_overview(config['username'], config['password'])
+    overview = fetch_overview(
+        config['churchname'],
+        config['username'],
+        config['password'])
     tree = html.fromstring(overview)
     people = tree.cssselect('.rota-subsection a.profile span.profile-name')
     for person in people:
