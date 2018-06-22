ec95a678556dccc61d6a692c726ab9055edf0204
Matthew Sital-Singh <mattss@netsight.co.uk>
2018-06-22 16:35:47 +0100

    Get script to update sunday numbers

--- sundayreview.py
+++ sundayreview.py
@@ -2,12 +2,19 @@
 import json
 import sys
 from datetime import datetime
+from string import ascii_letters
 
 import dateutil
 import drive
 from dateutil import relativedelta
 from lxml import html
 from masterrota import login
+
+SHEETS_ROOT_URL = 'https://docs.google.com/spreadsheets/d/'
+
+
+def get_timestamp():
+    return datetime.now().strftime('%Y-%m-%d %H:%M')
 
 
 def get_attendance(churchname, username, password, date, siteid=None):
@@ -74,6 +81,62 @@ def get_responses(sheetid, range_name, date):
     return responses
 
 
+def update_cell(sheetid, cellref, data):
+    body = {
+        'values': [
+            [data, ],
+        ]
+    }
+    service = drive.get_service()
+    service.spreadsheets().values().update(
+        spreadsheetId=sheetid,
+        body=body,
+        valueInputOption='USER_ENTERED',
+        range=cellref,
+    ).execute()
+
+
+def update_sheet_date(sheetid, thedate):
+    update_cell(
+        sheetid,
+        "'Last Sunday Summary'!B2",
+        get_timestamp(),
+    )
+    update_cell(
+        sheetid,
+        "'Last Sunday Summary'!B3",
+        thedate.strftime('%A %d %b %Y')
+    )
+
+
+def update_numbers(sheetid, numbers):
+    service = drive.get_service()
+    sheet_data = service.spreadsheets().values().get(
+        spreadsheetId=sheetid,
+        range="Last Sunday Summary",
+    ).execute()
+
+    for row_index, row in enumerate(sheet_data['values']):
+        if row[0] == 'Attendance':
+            targetrow = row_index + 2
+            for meeting in numbers:
+                if meeting['meeting_name'] in row:
+                    col = ascii_letters[row.index(
+                        meeting['meeting_name'])].upper()
+                    print('Writing {} to {}{}'.format(
+                        meeting['numbers'], col, targetrow,
+                    ))
+                    update_cell(
+                        sheetid,
+                        "'Last Sunday Summary'!{col}{row}".format(
+                            col=col,
+                            row=targetrow,
+                        ),
+                        meeting['numbers'],
+                    )
+            return
+
+
 if __name__ == "__main__":
     try:
         configfile = sys.argv[1]
@@ -90,6 +153,8 @@ if __name__ == "__main__":
             weekday=relativedelta.SU(-1))
     ).date()
 
+    last_sunday = datetime(2017, 9, 10)
+
     print('Sunday review for last sunday ({})'.format(
         last_sunday,
     ))
@@ -101,6 +166,8 @@ if __name__ == "__main__":
         last_sunday,
     )
 
+    sheetid = config['google_sheet_id']
+
     if not att:
         print('No attendance figures')
     for meeting in att:
@@ -110,8 +177,10 @@ if __name__ == "__main__":
             )
         )
 
+    update_numbers(sheetid, att)
+
     responses = get_responses(
-        config['response_google_sheet_id'],
+        sheetid,
         "Form responses 1",
         last_sunday,
     )
@@ -120,3 +189,10 @@ if __name__ == "__main__":
         print('Responses: ')
         for response in responses:
             print(', '.join(response))
+
+    update_sheet_date(
+        sheetid,
+        last_sunday,
+    )
+
+    print('Changes written to: {}{}'.format(SHEETS_ROOT_URL, sheetid))
